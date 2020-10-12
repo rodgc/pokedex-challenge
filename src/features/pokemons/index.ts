d861cbfadb1625e08a3df0222145ab3b271a3707
rodgc <rod.gc@hotmail.com>
2020-10-12 08:44:02 -0500

    Add index for Pokemon feat

--- src/features/pokemons/index.ts
+++ src/features/pokemons/index.ts
@@ -1,6 +1,6 @@
 import * as actionTypes from './actionTypes'
 import * as selectors from './selectors'
 
-export { default as CounterReducer } from './counterReducer'
+export { default as PokemonsReducer } from './pokemonsReducer'
 export { actionTypes }
 export { selectors }
