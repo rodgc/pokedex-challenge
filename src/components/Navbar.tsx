37816f5c366cc1946da1f5d5844de1c062907a92
rodgc <rod.gc@hotmail.com>
2020-10-12 12:30:49 -0500

    Fix nanvar not sticky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export const Navbar: React.FC = () => (
-  <nav>
-    <div className="nav-wrapper cyan darken-1 px1">
-      <NavLink to="/" className="brand-logo">
-        Pokedex
-      </NavLink>
-      <ul className="right hide-on-med-and-down">
-        <li cy-data="home-nav-link">
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/favorites">Favorites</NavLink>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <div className="navbar-fixed">
+    <nav>
+      <div className="nav-wrapper cyan darken-1 px1">
+        <NavLink to="/" className="brand-logo">
+          Pokedex
+        </NavLink>
+        <ul className="right hide-on-med-and-down">
+          <li cy-data="home-nav-link">
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/favorites">Favorites</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 )
